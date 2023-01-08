d1d838442613377b7430e8cc70251fab43e86b63
fernandojcm <fernandocamacho50@gmail.com>
2023-01-08 22:59:54 +0100

    done

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,38 +1,39 @@
 <template>
-  <div class="tweet">
-    <img
-      src="https://i.imgur.com/9yw1Fyw.jpg"
-      class="profile"
-      alt="profile"
-    />
+  <div className="tweet">
+    <ProfileImage :image="user.image"/>
 
-    <div class="body">
-      <div class="top">
-        <span class="user">
-          <span class="name">Ironhack</span>
-          <span class="handle">@ironhack</span>
-        </span>
-
-        <span class="timestamp">Nov 30, 2020</span>
+    <div className="body">
+      <div className="top">
+        <User :user="user" />
+        <TimeStamp :timestamp="timestamp" />
       </div>
+      <div className="message">
+        <Message :message="message" />
+      </div>
+      
 
-      <p class="message">
-        On December 7th, we will be hosting a #webinar that will introduce you
-        to #SQL! Are you ready? 🚀
-      </p>
-
-      <div class="actions">
-        <!-- Font Awesome icons -->
-        <i class="far fa-comment"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="far fa-heart"></i>
-        <i class="fas fa-share"></i>
+      <div>
+        <Actions />
       </div>
     </div>
 
     <i class="fas fa-ellipsis-h"></i>
   </div>
 </template>
+
+<script>
+
+import ProfileImage from './ProfileImage.vue';
+import User from './User.vue';
+import TimeStamp from './TimeStamp.vue';
+import Message from './Message.vue';
+import Actions from './Actions.vue';
+
+export default {
+    props: ["timestamp", "message", "user"],
+    components: { ProfileImage, User, TimeStamp, Message, Actions }
+};
+</script>
 
 <style scoped>
 a {
---
+++ src/components/User.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <span className="user">
+            <span className="name"> {{ user.name }}</span>
+            <span className="handle">@{{ user.handle }}</span>
+          </span>
+    </div>
+  </template>
+  
+
+<script>
+
+export default {
+    props: ["user"],
+}
+
+</script>
+
+<style>
+</style>
